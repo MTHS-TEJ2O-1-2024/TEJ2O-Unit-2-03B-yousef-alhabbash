c79634efcb8adbd8d8f4e5ed8366246ac709ff11
yousef-alhabbash <yousef.al-habbash@mths.ca>
2024-09-22 19:23:47 -0400

    Perimeter and area

--- main.ts
+++ main.ts
@@ -1,8 +1,15 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Mr. Yousef
+ * Created on: Sep 2024
+ * This program is about doing simple math
 */
 
-basic.showString('Hello, World!')
+basic.clearScreen()
+basic.pause(1000)
+
+let area = (5*3);
+basic.showString('area is = ' + area.toString() + ' cm^2')
+let perimeter = 2 * (5+3)
+basic.showString('perimeter is = ' + area.toString() + ' cm')
+
